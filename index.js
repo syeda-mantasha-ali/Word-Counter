e568032d3d235b5704e98ba609b9c64768c80b15
Syeda Mantasha Ali <syedamantashaali6@gmail.com>
2024-04-22 17:20:36 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words: ",
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
